d4ef1bc2eb4ee854d2a1471f985adaa998e2359e
djnautiyal <dhannanjainautiyal@gmail.com>
2024-03-12 02:11:04 +0530

    more product options

--- src/main/java/com/learnjava/service/ProductInfoService.java
+++ src/main/java/com/learnjava/service/ProductInfoService.java
@@ -12,7 +12,9 @@ public class ProductInfoService {
     public ProductInfo retrieveProductInfo(String productId) {
         delay(1000);
         List<ProductOption> productOptions = List.of(new ProductOption(1, "64GB", "Black", 699.99),
-                new ProductOption(2, "128GB", "Black", 749.99));
+                new ProductOption(2, "128GB", "Grey", 749.99),
+                new ProductOption(3, "256GB", "White", 899.99),
+                new ProductOption(4, "512GB", "Metallic", 1999.99));
         return ProductInfo.builder().productId(productId)
                 .productOptions(productOptions)
                 .build();
